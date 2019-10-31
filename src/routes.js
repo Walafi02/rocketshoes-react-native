bb410c249dd33afe0c962d4edf429c4b58adb8e5
walafi <walafiferreira@trixlog.com>
2019-10-31 17:58:31 -0300

    seting the transitionConfig from right

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {fromRight} from 'react-navigation-transitions';
 
 import Cart from './pages/Cart';
 import Home from './pages/Home';
@@ -17,6 +18,7 @@ const Routes = createAppContainer(
       defaultNavigationOptions: navigation => ({
         header: <Header {...navigation} />,
       }),
+      transitionConfig: () => fromRight(),
       cardStyle: {
         backgroundColor: '#000',
       },

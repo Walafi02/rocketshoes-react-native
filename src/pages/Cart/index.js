8a47132c4e18b3be1a325cfb1e1e16628a1f102c
Walafi02 <walafif@yahoo.com>
2019-10-31 23:24:38 -0300

    implementing Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+// import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Title} from './styles';
+import {
+  Container,
+  Product,
+  ProductMain,
+  ProductFooter,
+  ProductImage,
+  ProductMainTexts,
+  ProductMainTitle,
+  ProductMainPrice,
+  ProductDelete,
+} from './styles';
 
 export default function Cart() {
   return (
-    <View>
-      <Title>Page Cart</Title>
-    </View>
+    <Container>
+      <Product>
+        <ProductMain>
+          <ProductImage
+            source={{
+              uri:
+                'https://static.zattini.com.br/produtos/sapato-casual-em-couro-d&r-shoes-masculino/06/FAO-0332-006/FAO-0332-006_zoom1.jpg',
+            }}
+          />
+          <ProductMainTexts>
+            <ProductMainTitle>sjdiaiusia dk djsadja</ProductMainTitle>
+            <ProductMainPrice>120,20</ProductMainPrice>
+          </ProductMainTexts>
+        </ProductMain>
+        <ProductDelete>
+          <Icon name="delete-forever" size={24} color="#715fc1" />
+        </ProductDelete>
+        <ProductFooter />
+      </Product>
+    </Container>
   );
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,7 +1,52 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  width: auto;
+  background: #fff;
+  margin: 20px;
+  padding: 10px;
+  border-radius: 7px;
+`;
 
 export const Title = styled.Text`
   color: red;
 `;
+
+export const Product = styled.View`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ProductMain = styled.View`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ProductImage = styled.Image`
+  height: 100px;
+  width: 100px;
+`;
+
+export const ProductMainTexts = styled.View`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const ProductDelete = styled.TouchableOpacity`
+  justify-content: center;
+  margin-left: auto;
+`;
+
+export const ProductMainTitle = styled.Text.attrs({
+  numberOfLines: 2, // define quantas linas o texto pode ter
+})`
+  font-size: 22px;
+  color: #191920;
+`;
+
+export const ProductMainPrice = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const ProductFooter = styled.View``;
